669f439e62288f6897b370b37ffeccf1d878d59e
AndyP <firreatwill@mail.ru>
2023-10-08 17:51:14 +0300

    create model and data

--- FriendsList/Models/Persons.swift
+++ FriendsList/Models/Persons.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getFriendsList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
--- FriendsList/Services/DataStore.swift
+++ FriendsList/Services/DataStore.swift
@@ -5,4 +5,61 @@
 //  Created by Андрей Платунов on 08.10.2023.
 //
 
-import Foundation
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let names = [
+        "Bisbi",
+        "Peter",
+        "Pepe",
+        "Tod",
+        "Mailo",
+        "Shevrone",
+        "Tasha",
+        "Geel",
+        "Bee",
+        "Boris"
+    ]
+    
+    let surnames = [
+        "Pasta",
+        "Flock",
+        "Leanore",
+        "Cunt",
+        "Jopper",
+        "Anna-Maria",
+        "Cat",
+        "Chop-Chop",
+        "Ikari",
+        "Langock"
+    ]
+    
+    let emails = [
+        "Cat@mail.ru",
+        "Seed@mail.ru",
+        "Cool@mail.ru",
+        "Man@mail.ru",
+        "Manny@mail.ru",
+        "Kertis@mail.ru",
+        "Taps@mail.ru",
+        "Olin@mail.ru",
+        "Pussy@mail.ru",
+        "Drow@mail.ru"
+    ]
+    
+    let phones = [
+        "456795999",
+        "987456712",
+        "345009785",
+        "999888777",
+        "142568956",
+        "908567465",
+        "111111111",
+        "000989764",
+        "876444098",
+        "123456789"
+    ]
+    
+    private init() {}
+}
