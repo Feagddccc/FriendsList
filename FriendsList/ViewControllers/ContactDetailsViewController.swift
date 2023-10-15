56ce4a74541310b602b04e33de8d22256baa9d8d
AndyP <firreatwill@mail.ru>
2023-10-15 19:39:21 +0300

    ffs xcode 15

---
+++ FriendsList/ViewControllers/ContactDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactDetailsViewController.swift
+//  FriendsList
+//
+//  Created by Андрей Платунов on 10.10.2023.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FriendsList/ViewControllers/SectionTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SectionTableViewController.swift
+//  FriendsList
+//
+//  Created by Андрей Платунов on 09.10.2023.
+//
+
+import UIKit
+
+class SectionTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
